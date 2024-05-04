d102e69abaf024c30c164552f16fb974ef6897f0
444B <15311579+444B@users.noreply.github.com>
2024-05-04 11:56:45 +0200

    adding ai investigator

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def log_meal():
         datetime_entry = datetime.combine(meal_date, meal_time)
         new_entry = {'timestamp': datetime_entry, 'meal_type': meal_type, 'food_consumed': food_consumed, 'optional_notes': optional_notes}
         new_entry_df = pd.DataFrame([new_entry])
-        new_entry_df.to_csv("food_entries.csv", mode='a', header=not os.path.exists("food_entries.csv"), index=False)
+        new_entry_df.to_csv("data/food_entries.csv", mode='a', header=not os.path.exists("data/food_entries.csv"), index=False)
         st.success("Meal logged successfully!")
 
 
@@ -49,7 +49,7 @@ def log_symptom():
         datetime_entry = datetime.combine(symptom_date, symptom_time)
         new_symptom = {'timestamp': datetime_entry, 'symptom_type': symptom_type, 'severity': severity, 'notes': symptom_notes}
         new_symptom_df = pd.DataFrame([new_symptom])
-        new_symptom_df.to_csv("symptoms.csv", mode='a', header=not os.path.exists("symptoms.csv"), index=False)
+        new_symptom_df.to_csv("data/symptoms.csv", mode='a', header=not os.path.exists("data/symptoms.csv"), index=False)
         st.success("Symptom logged successfully!")
 
 # Data Visualization and Analysis
@@ -106,19 +106,76 @@ def visualize_data(meal_data, symptoms_data):
     ax.grid(True)
     st.pyplot(fig)
 
+def ai_consultation():
+    st.header("AI Dietician")
+    if st.checkbox("run code?"):
+        import openai
+        import streamlit as st
+        from llama_index.llms.openai import OpenAI
+        try:
+            from llama_index import (Document, ServiceContext,
+                                    SimpleDirectoryReader, VectorStoreIndex)
+        except ImportError:
+            from llama_index.core import (Document, ServiceContext,
+                                        SimpleDirectoryReader,
+                                        VectorStoreIndex)
+
+        # st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+        openai.api_key = st.secrets.openai_key
+        st.title("Chat with this websites Blog posts, powered by LlamaIndex")                
+        if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+            st.session_state.messages = [
+                {"role": "assistant", "content": "Ask me a question about this websites blog posts!"}
+            ]
+
+        @st.cache_resource(show_spinner=False)
+        def load_data():
+            with st.spinner(text="Loading and indexing the data – hang tight! This should take a few seconds."):
+                reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+                docs = reader.load_data()
+                # llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert o$
+                # index = VectorStoreIndex.from_documents(docs)
+                service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You answer questions on post content and highlight interesting points - do not hallucinate posts or opinions."))
+                index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+                return index
+
+        index = load_data()
+
+        if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+                st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+        if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+
+        for message in st.session_state.messages: # Display the prior chat messages
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
+
+        # If last message is not from assistant, generate a new response
+        if st.session_state.messages[-1]["role"] != "assistant":
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    response = st.session_state.chat_engine.chat(prompt)
+                    st.write(response.response)
+                    message = {"role": "assistant", "content": response.response}
+                    st.session_state.messages.append(message) # Add response to message history
+
+
 
 
 # UI
 def main():
-        tab1, tab2, tab3 = st.tabs(["Meal Entry Form", "Symptoms Logging", "Data Visualization and Analysis"])
+        tab1, tab2, tab3, tab4= st.tabs(["Meal Entry Form", "Symptoms Logging", "Data Visualization and Analysis"])
         with tab1:
             log_meal()
         with tab2:
             log_symptom()
         with tab3:
-            meal_data = pd.read_csv("food_entries.csv")
-            symptoms_data = pd.read_csv("symptoms.csv")
+            meal_data = pd.read_csv("data/food_entries.csv")
+            symptoms_data = pd.read_csv("data/symptoms.csv")
             visualize_data(meal_data, symptoms_data)
+        with tab4:
+            ai_consultation()
 
 if __name__ == "__main__":
     main()
